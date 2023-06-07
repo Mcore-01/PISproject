5f445bd1821e7d83640a2151e72a879d490bface
a7comma <iba7comma@gmail.com>
2023-06-07 11:32:29 +0500

    Добавлен экспорт контрактов

--- pisV228.4/MunicipalContractRegisterForm.Designer.cs
+++ pisV228.4/MunicipalContractRegisterForm.Designer.cs
@@ -32,6 +32,7 @@
             this.MContractRegisterGBox = new System.Windows.Forms.GroupBox();
             this.OpenMCRButton = new System.Windows.Forms.Button();
             this.AddMCRButton = new System.Windows.Forms.Button();
+            this.ExportButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MCRDataGridView)).BeginInit();
             this.MContractRegisterGBox.SuspendLayout();
             this.SuspendLayout();
@@ -85,11 +86,25 @@
             this.AddMCRButton.Text = "Добавить";
             this.AddMCRButton.UseVisualStyleBackColor = false;
             // 
+            // ExportButton
+            // 
+            this.ExportButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ExportButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExportButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ExportButton.Location = new System.Drawing.Point(564, 259);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(127, 50);
+            this.ExportButton.TabIndex = 2;
+            this.ExportButton.Text = "Экспортировать в Excel";
+            this.ExportButton.UseVisualStyleBackColor = false;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
             // MunicipalContractRegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 550);
+            this.Controls.Add(this.ExportButton);
             this.Controls.Add(this.MContractRegisterGBox);
             this.Controls.Add(this.MCRDataGridView);
             this.Name = "MunicipalContractRegisterForm";
@@ -107,5 +122,6 @@
         private System.Windows.Forms.GroupBox MContractRegisterGBox;
         private System.Windows.Forms.Button OpenMCRButton;
         private System.Windows.Forms.Button AddMCRButton;
+        private System.Windows.Forms.Button ExportButton;
     }
 }
